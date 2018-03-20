1577e2aed71230161b5ca5a4c77cc9c0987029a5
Robin Reicher <Robin.Reicher@westermo.se>
2018-03-20 19:54:05 +0100

    First humble commit
    Signed-off-by: Robin Reicher <Robin.Reicher@westermo.se>

---
+++ login.gtpl
@@ -0,0 +1,12 @@
+<html>
+    <head>
+    <title>Practice</title>
+    </head>
+    <body>
+        <form action="/practice" method="post">
+            <input type="submit" name="Choice" value="Rock" parameter>
+            <input type="submit" name="Choice" value="Paper">
+            <input type="submit" name="Choice" value="Scissors">
+        </form>
+    </body>
+</html>
---
+++ web.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() //Parse url parameters passed, then parse the response packet for the POST body (request body)
+	// attention: If you do not call ParseForm method, the following data can not be obtained form
+	fmt.Println(r.Form) // print information on server side.
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "RPS8000!") // write data to response
+}
+
+func practice(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //get request method
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		// logic part of log in
+		fmt.Println("User choice: ", r.Form["Choice"])
+	}
+}
+
+func main() {
+	http.HandleFunc("/", sayhelloName) // setting router rule
+	http.HandleFunc("/practice", practice)
+	err := http.ListenAndServe(":9090", nil) // setting listening port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
