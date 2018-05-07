b3500e3647eb2ae63d9e2aff23d5e575e57405a8
Robin Reicher <Robin.Reicher@westermo.se>
2018-05-07 18:17:09 +0200

    Fixed double-output and rolled back last commit :P
    Signed-off-by: Robin Reicher <Robin.Reicher@westermo.se>

--- web.go
+++ web.go
@@ -10,30 +10,6 @@ import (
 	"github.com/reicher/RPS7000/gesture"
 )
 
-func startpage(w http.ResponseWriter, r *http.Request) {
-
-	// ME: Not really needed? Only for debugging
-	r.ParseForm() //Parse url parameters passed, then parse the response packet for the POST body (request body)
-
-	// // attention: If you do not call ParseForm method, the following data can not be obtained form
-	fmt.Println(r.Form) // print information on server side.
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-
-	// Actuall page load
-	t, err := template.ParseFiles("~/go/src/github.com/reicher/RPS7000/assets/startpage.gtpl")
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		t.Execute(w, nil)
-	}
-}
-
 func randomAI() int {
 	return rand.Intn(3)
 }
@@ -43,8 +19,11 @@ type PracticePageData struct {
 }
 
 func practice(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("~/go/src/github.com/reicher/RPS7000/assets/practice.gtpl")
-	t.Execute(w, nil)
+	t, _ := template.ParseFiles("/home/rrr/go/src/github.com/reicher/RPS7000/assets/practice.gtpl")
+
+	data := PracticePageData{
+		PageTitle: "",
+	}
 
 	if r.Method == "POST" {
 		r.ParseForm()
@@ -63,12 +42,36 @@ func practice(w http.ResponseWriter, r *http.Request) {
 			result +=" AI Wins!"
 		}
 
-		data := PracticePageData{
-			PageTitle: result,
-		}
-		t.Execute(w, data)
+		data.PageTitle = result
+
+
 
 		fmt.Println(result)
+	}
+	t.Execute(w, data)
+}
+
+func startpage(w http.ResponseWriter, r *http.Request) {
+
+	// ME: Not really needed? Only for debugging
+	r.ParseForm() //Parse url parameters passed, then parse the response packet for the POST body (request body)
+
+	// // attention: If you do not call ParseForm method, the following data can not be obtained form
+	fmt.Println(r.Form) // print information on server side.
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+
+	// Actuall page load
+	t, err := template.ParseFiles("/home/rrr/go/src/github.com/reicher/RPS7000/assets/startpage.gtpl")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		t.Execute(w, nil)
 	}
 }
 
@@ -76,6 +79,7 @@ func main() {
 	fmt.Println("localhost:9090")
 
 	rand.Seed(40) // Try changing this number!
+
 	http.HandleFunc("/", startpage) // setting router rule
 	http.HandleFunc("/practice", practice)
 	err := http.ListenAndServe(":9090", nil) // setting listening port
